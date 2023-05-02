cee05572fdeaf090c8097bdc8cb1c00f7f32a593
Tushar <freelancetushar@gmail.com>
2023-05-02 11:59:00 +0530

    Update status

--- SampleApps/ViewController.swift
+++ SampleApps/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import WebKit
+import MessageUI
 import SafariServices
 
 class ViewController: UIViewController, WKNavigationDelegate, SFSafariViewControllerDelegate {
@@ -39,6 +40,41 @@ class ViewController: UIViewController, WKNavigationDelegate, SFSafariViewContro
         }
     }
     
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction,
+                     decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+            guard
+                let url = navigationAction.request.url else {
+                    decisionHandler(.cancel)
+                    return
+            }
+            
+            let string = url.absoluteString
+            if (string.contains("mailto:")) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                decisionHandler(.cancel)
+                return
+            } else if (string.contains("https://avibra.app.link") || string.contains("https://avibra.test-app.link") || string.contains("https://avibra-alternate.test-app.link") || string.contains("https://avibra-alternate.app.link")) {
+                if (UIApplication.shared.canOpenURL(url)) {
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                }
+                decisionHandler(.cancel)
+            } else if (string.contains("https://apps.apple.com/us/app/avibra/id1449843334")) {
+                if (UIApplication.shared.canOpenURL(url)) {
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                }
+                decisionHandler(.cancel)
+            } else if (string.contains("https://apps.apple.com/us/app/avibra/id1449843334")) {
+                let itunesURL = URL(string: "itms-apps://itunes.apple.com/app/id1449843334") ?? url;
+                if (UIApplication.shared.canOpenURL(url)) {
+                    UIApplication.shared.open(itunesURL, options: [:], completionHandler: nil)
+                }
+                decisionHandler(.cancel)
+            }
+            decisionHandler(.allow)
+        }
+    
+    
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         
     }
@@ -55,6 +91,17 @@ class ViewController: UIViewController, WKNavigationDelegate, SFSafariViewContro
                 UIApplication.shared.open(url)
             }
             webView.goBack();
+        } else if (urlStr.hasPrefix("https://avibra-alternate.test-app.link") || urlStr.hasPrefix("https://avibra-alternate.app.link")) {
+            if (UIApplication.shared.canOpenURL(url)) {
+                UIApplication.shared.open(url)
+            }
+        } else if (urlStr.hasPrefix("https://apps.apple.com/us/app/avibra/id1449843334")) {
+            if let url = URL(string: "itms-apps://itunes.apple.com/app/id1449843334") {
+                UIApplication.shared.open(url)
+            }
+        } else if (urlStr.hasPrefix("mailto:")) {
+            let mc: MFMailComposeViewController = MFMailComposeViewController();
+            self.present(mc, animated: true);
         }
     }
     
